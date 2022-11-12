53d24b89217bbe1a769499f9fa06106e504bbe7d
Rickster Muyco <rickslinger06@gmail.com>
2022-11-11 19:57:42 -0500

    version2Test

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -9,90 +9,87 @@ import java.util.Scanner;
 
 public class Runner {
 
-	public static void main(String[] args) throws IOException{
-	
-	System.out.println(countChar('a',"file.txt"));
-	readFile("file.txt");
-	userRegistration("file.txt");
-	
-	readFile("file.txt");
-	
+	public static void main(String[] args) throws IOException {
+
+		System.out.println(countChar('a', "file.txt"));
+		readFile("file.txt");
+		userRegistration("file.txt");
+
+		readFile("file.txt");
 
 	}
-	
-public static int countChar(char c ,String filename){
 
-	int count = 0;
-	try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-		while (br.ready()) {
-			String line = br.readLine();
-			char[] txtArray = line.toCharArray();
+	public static int countChar(char c, String filename) {
 
-			for (char i : txtArray) {
-				if (i == c) {
-					count++;
+		int count = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			while (br.ready()) {
+				String line = br.readLine();
+				char[] txtArray = line.toCharArray();
+
+				for (char i : txtArray) {
+					if (i == c) {
+						count++;
+					}
 				}
 			}
+
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return count;
+
+	}
+
+	// UserRegistration ----
+
+	public static void userRegistration(String file) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter Your name: ");
+		String name = input.nextLine();
+
+		System.out.println("Enter Your Address: ");
+		String address = input.nextLine();
+
+		System.out.println("Enter Your E-mail: ");
+		String eMAil = input.nextLine();
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
+			bw.append(name + ",");
+			bw.append(address + ",");
+			bw.append(eMAil + ",");
+			bw.newLine();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
 		}
 
-	} catch (FileNotFoundException e) {
+		input.close();
 
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-	return count;
-
-}
-
-public static void userRegistration(String file) {
-	
-	Scanner input = new Scanner(System.in);
-	
-	
-	
-	System.out.println("Enter Your name: ");
-	String name = input.nextLine();
-	
-	System.out.println("Enter Your Address: ");
-	String address = input.nextLine();
-	
-	System.out.println("Enter Your E-mail: ");
-	String eMAil = input.nextLine();
-	
-	
-	try (BufferedWriter bw = new BufferedWriter(new FileWriter(file ,true))) {
-		bw.append(name+",");
-		bw.append(address+",");
-		bw.append(eMAil + ",");
-		bw.newLine();
-		
-	} catch (IOException e) {
-		
-		e.printStackTrace();
-		
 	}
 
-	
-	input.close();
-	
-}
 	public static void readFile(String file) {
 
-	try(BufferedReader br = new BufferedReader(new FileReader(file))) {
-		while(br.ready()) {
-			String line = br.readLine();
-			System.out.println(line);
-		}
-		
-	} catch (FileNotFoundException e) {
-		
-		e.printStackTrace();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-}
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			while (br.ready()) {
+				String line = br.readLine();
+				System.out.println(line);
+			}
 
-	
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+
 }
